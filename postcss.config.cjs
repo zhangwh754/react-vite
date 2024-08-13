596998fc612cacfacf844dcd4368897a55fc40aa
zhang <zhangwh754@163.com>
2024-08-13 23:07:04 +0800

    chore: screen fit with postcss-px-to-viewport-8-plugin

---
+++ postcss.config.cjs
@@ -0,0 +1,10 @@
+const config = {
+  plugins: {
+    'postcss-preset-env': {},
+    'postcss-px-to-viewport-8-plugin': {
+      viewportWidth: 1920,
+    },
+  },
+}
+
+module.exports = config
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import 'normalize.css'
+import './assets/base.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
