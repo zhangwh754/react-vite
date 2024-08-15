b0913623ac29292a24a7fa66d8f31cede1bc57cb
zhang <zhangwh754@163.com>
2024-08-15 23:36:21 +0800

    fix: add Suspense to fix lazy load problem

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import 'normalize.css'
@@ -6,6 +6,8 @@ import './assets/base.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Suspense>
+      <App />
+    </Suspense>
   </StrictMode>
 )
