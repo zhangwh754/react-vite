72c5197cf97f2b02d6524751f283c07b659255ec
zhang <zhangwh754@163.com>
2024-08-16 19:04:24 +0800

    feat: update Manage Layout

--- src/layout/ManageLayout/index.tsx
+++ src/layout/ManageLayout/index.tsx
@@ -1,28 +1,33 @@
 import React, { FC } from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { Space, Button, Divider } from 'antd'
+import { BarChartOutlined, DeleteOutlined, PlusOutlined, StarOutlined } from '@ant-design/icons'
 import styles from './style.module.scss'
+import { MANAGE_INDEX, MANAGE_STAR, MANAGE_TRASH } from '@/router'
 
 type PropTypes = {}
 
 const ManageLayout: FC<PropTypes> = () => {
+  const nav = useNavigate()
+
   return (
     <>
       <div className={styles['manage-layout']}>
-        <div className={styles['manage-panel']}>
-          <button>新建问卷</button>
-
-          <div className={styles['manage-navigation']}>
-            <a className={styles['link']} href="#">
-              我的问卷
-            </a>
-            <a className={styles['link']} href="#">
-              标星问卷
-            </a>
-            <a className={styles['link']} href="#">
-              回收站
-            </a>
-          </div>
-        </div>
+        <Space className={styles['manage-panel']} align="end" direction="vertical">
+          <Button icon={<PlusOutlined />} type="primary">
+            新建问卷
+          </Button>
+          <Divider />
+          <Button icon={<BarChartOutlined />} onClick={() => nav(MANAGE_INDEX)}>
+            我的问卷
+          </Button>
+          <Button icon={<StarOutlined />} onClick={() => nav(MANAGE_STAR)}>
+            标星问卷
+          </Button>
+          <Button icon={<DeleteOutlined />} onClick={() => nav(MANAGE_TRASH)}>
+            回收站
+          </Button>
+        </Space>
         <div className={styles['manage-content']}>
           <Outlet></Outlet>
         </div>
