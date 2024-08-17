73cb24b2017319361cd4839683045eba604eb88d
zhang <zhangwh754@163.com>
2024-08-17 09:49:10 +0800

    chore: eslint规则调整，index.html语言修改

--- eslint.config.js
+++ eslint.config.js
@@ -22,5 +22,6 @@ export default tseslint.config({
     ...reactHooks.configs.recommended.rules,
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     '@typescript-eslint/no-empty-object-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
 })
