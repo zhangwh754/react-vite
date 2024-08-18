7d5af82dc4a22b7ed65df7959fc55ffcc4aab5fa
zhang <zhangwh754@163.com>
2024-08-18 09:38:54 +0800

    chore: config devServer proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,33 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "@/assets/mixin.scss" as *;`,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
-  },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/assets/mixin.scss" as *;`,
+        },
+      },
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_HTTP_BASE_URL,
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  }
 })
