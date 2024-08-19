00774447345d1076b13ac5488df6fedb5be5e40a
zhang <zhangwh754@163.com>
2024-08-19 23:30:36 +0800

    feat: 获取用户信息

--- src/components/UserInfo/index.tsx
+++ src/components/UserInfo/index.tsx
@@ -1,16 +1,44 @@
 import { LOGIN_URL } from '@/router'
-import React, { FC } from 'react'
-import { Link } from 'react-router-dom'
+import React, { FC, useEffect } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import styles from './style.module.scss'
+import { useRequest } from 'ahooks'
+import { getUser } from '@/network'
+import { Avatar, Button, Space } from 'antd'
 
 type PropTypes = {}
 
 const UserInfo: FC<PropTypes> = () => {
+  const { loading, data, error, run } = useRequest(getUser, { manual: true })
+  const { pathname } = useLocation()
+  const nav = useNavigate()
+
+  useEffect(() => {
+    if (pathname.includes('/login') || pathname.includes('/register')) return
+
+    run()
+  }, [pathname])
+
   return (
     <>
-      <Link className={styles.link} to={LOGIN_URL}>
-        登录
-      </Link>
+      {!loading && data ? (
+        <Space align="center">
+          <Avatar
+            style={{ backgroundColor: '#f56a00', verticalAlign: 'middle' }}
+            size="large"
+            gap={4}
+          >
+            {data.nickname}
+          </Avatar>
+          <Button type="link" onClick={() => nav(LOGIN_URL)}>
+            登出
+          </Button>
+        </Space>
+      ) : (
+        <Link className={styles.link} to={LOGIN_URL}>
+          登录
+        </Link>
+      )}
     </>
   )
 }
