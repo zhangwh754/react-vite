3f102cae3d92b7273bb0b1ad48c9ef52172e36ae
zhang <zhangwh754@163.com>
2024-08-20 23:19:53 +0800

    chore: edit eslint rule

--- eslint.config.js
+++ eslint.config.js
@@ -23,5 +23,6 @@ export default tseslint.config({
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     '@typescript-eslint/no-empty-object-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
   },
 })
