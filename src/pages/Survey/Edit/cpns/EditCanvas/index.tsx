36f2f8c061d9a204d9851b270401640de379aa7d
zhang <zhangwh754@163.com>
2024-08-29 22:50:07 +0800

    refactor: Edit页面组件重新拆分

---
+++ src/pages/Survey/Edit/cpns/EditCanvas/index.tsx
@@ -0,0 +1,61 @@
+import React, { FC, MouseEvent } from 'react'
+import { useDispatch } from 'react-redux'
+import { Spin } from 'antd'
+import classNames from 'classnames'
+import styles from './style.module.scss'
+import { setSelectedComponentId } from '@/store/component/componentReducer'
+import useGetSurveyDetailInfo from '@/hooks/useGetSurveyDetailInfo'
+import { getComponentConfigByType } from '@/components/SurveyComponent'
+
+type PropTypes = {
+  loading: boolean
+}
+
+const EditCanvas: FC<PropTypes> = props => {
+  const { loading } = props
+
+  const dispatch = useDispatch()
+
+  const { componentsList, selectedComponentId } = useGetSurveyDetailInfo()
+
+  const onComponentClick = (e: MouseEvent, id: string) => {
+    e.stopPropagation()
+
+    dispatch(setSelectedComponentId(id))
+  }
+
+  return (
+    <>
+      {loading ? (
+        <div style={{ textAlign: 'center' }}>
+          <Spin></Spin>
+        </div>
+      ) : (
+        <div className={styles['canvas-container']}>
+          {componentsList.map(item => {
+            const { id, componentType, props } = item
+
+            const { Component } = getComponentConfigByType(componentType)
+
+            return (
+              <div
+                key={id}
+                className={classNames({
+                  [`${styles['canvas-row']}`]: true,
+                  [`${styles.selected}`]: id === selectedComponentId,
+                })}
+                onClick={e => onComponentClick(e, id)}
+              >
+                <div className={styles['canvas-item']}>
+                  <Component {...props}></Component>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      )}
+    </>
+  )
+}
+
+export default EditCanvas
--- src/pages/Survey/Edit/index.tsx
+++ src/pages/Survey/Edit/index.tsx
@@ -1,19 +1,37 @@
 import React, { FC } from 'react'
+import { useDispatch } from 'react-redux'
 import Header from './cpns/Header'
-import Content from './cpns/Content'
 import styles from './style.module.scss'
 import useGetLoadingSurveyDetailData from '@/hooks/useGetLoadingSurveyDetailData'
+import SurveyComponentList from './cpns/SurveyComponentList'
+import { setSelectedComponentId } from '@/store/component/componentReducer'
+import EditProps from './cpns/EditProps'
+import EditCanvas from './cpns/EditCanvas'
 
 type PropTypes = {}
 
 const Edit: FC<PropTypes> = () => {
   const { loading, error } = useGetLoadingSurveyDetailData()
 
+  const dispatch = useDispatch()
+
   return (
     <div className={styles.wrapper}>
       <Header></Header>
 
-      <Content loading={loading}></Content>
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <SurveyComponentList></SurveyComponentList>
+        </div>
+
+        <div className={styles.middle} onClick={() => dispatch(setSelectedComponentId(''))}>
+          <EditCanvas loading={loading}></EditCanvas>
+        </div>
+
+        <div className={styles.right}>
+          <EditProps></EditProps>
+        </div>
+      </div>
     </div>
   )
 }
