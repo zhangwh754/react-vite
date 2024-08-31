e07d3dd9c6a197a081ec7cc751933bc265543250
zhang <zhangwh754@163.com>
2024-08-31 09:48:25 +0800

    feat: 快捷操作-锁定/隐藏

--- src/pages/Survey/Edit/cpns/EditHeader/index.tsx
+++ src/pages/Survey/Edit/cpns/EditHeader/index.tsx
@@ -1,15 +1,31 @@
 import React, { ChangeEvent, FC, useState } from 'react'
 import styles from './style.module.scss'
-import { Input, Space } from 'antd'
-import { LeftOutlined } from '@ant-design/icons'
+import { Button, Input, Space } from 'antd'
+import {
+  CheckOutlined,
+  EyeInvisibleOutlined,
+  EyeOutlined,
+  LeftOutlined,
+  LockOutlined,
+  SaveOutlined,
+  UnlockOutlined,
+} from '@ant-design/icons'
 import { useNavigate } from 'react-router-dom'
+import useGetSurveyDetailInfo from '@/hooks/useGetSurveyDetailInfo'
+import { useDispatch } from 'react-redux'
+import { setComponentHideStatus, setComponentLockStatus } from '@/store/component/componentReducer'
 
 type PropTypes = {}
 
 const EditHeader: FC<PropTypes> = () => {
   const [title, setTitle] = useState('问卷标题1')
 
+  const { selectedComponent, selectedComponentId } = useGetSurveyDetailInfo()
+
+  const { isHide = false, isLock = false } = selectedComponent || {}
+
   const nav = useNavigate()
+  const dispatch = useDispatch()
 
   const onBlur = () => {
     console.log('currentTitle: ', title)
@@ -19,6 +35,14 @@ const EditHeader: FC<PropTypes> = () => {
     const val = e.target.value
 
     setTitle(val)
+  }
+
+  const onToggleLock = () => {
+    dispatch(setComponentLockStatus({ id: selectedComponentId, isLock: !isLock }))
+  }
+
+  const onToggleHide = () => {
+    dispatch(setComponentHideStatus({ id: selectedComponentId, isHide: !isHide }))
   }
 
   return (
@@ -39,8 +63,30 @@ const EditHeader: FC<PropTypes> = () => {
             />
           </Space>
         </div>
-        <div className={styles.middle}></div>
-        <div className={styles.right}></div>
+        <div className={styles.middle}>
+          <Space>
+            <Button
+              shape="circle"
+              disabled={selectedComponentId === ''}
+              icon={isLock ? <UnlockOutlined /> : <LockOutlined />}
+              onClick={onToggleLock}
+            />
+            <Button
+              shape="circle"
+              disabled={selectedComponentId === ''}
+              icon={isHide ? <EyeOutlined /> : <EyeInvisibleOutlined />}
+              onClick={onToggleHide}
+            />
+          </Space>
+        </div>
+        <div className={styles.right}>
+          <Space>
+            <Button icon={<CheckOutlined />}>发布</Button>
+            <Button icon={<SaveOutlined />} type="primary">
+              保存
+            </Button>
+          </Space>
+        </div>
       </div>
     </>
   )
